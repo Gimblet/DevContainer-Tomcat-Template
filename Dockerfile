c54aeea283df2070fc948358bf741d40cabf59e1
Gimblet <gimbletdev@gmail.com>
2024-08-11 02:06:55 +0000

    Completition Works

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,2 @@
 FROM tomcat:9.0.91-jdk17-temurin-noble
-EXPOSE "8888:808"
+EXPOSE "8888:8080"
--- src/main/java/servlets/Servlet.java
+++ src/main/java/servlets/Servlet.java
@@ -7,27 +7,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Servlet
- */
 @WebServlet("/Servlet")
 public class Servlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
-    public Servlet() {
-        // TODO Auto-generated constructor stub
-    }
+	public Servlet() {
 
-	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String nombre = request.getParameterByName("txttexto");
+	}
+
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String nombre = request.getParameter("txttexto");
 		request.getRequestDispatcher("hola.jsp").forward(request, response);
 		request.setAttribute("key", nombre);
 	}
-
 }
